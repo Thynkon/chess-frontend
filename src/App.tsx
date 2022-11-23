0562b03d38991878f3ef44e4c7ad41e50ad0a40c
thynkon <thynkon@protonmail.com>
2022-11-23 19:03:34 +0100

    Install chessboard react wrapper and chess.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
+import Chessground from "@react-chess/chessground";
+
 import logo from './logo.svg';
+
 import './App.css';
+import "chessground/assets/chessground.base.css";
+import "chessground/assets/chessground.brown.css";
+import "chessground/assets/chessground.cburnett.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chessground />
     </div>
   );
 }
