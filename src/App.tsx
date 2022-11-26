0705e6a4ec34f21043b2c938c182f793831b957f
thynkon <thynkon@protonmail.com>
2022-11-26 21:32:28 +0100

    Log piece's PGN when moving them

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Chessground from "@react-chess/chessground";
+import { parseFen } from 'chessops/fen';
+import { Chess } from 'chessops/chess';
 
 import logo from './logo.svg';
 
@@ -7,11 +9,30 @@ import './App.css';
 import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
+import { Config } from 'chessground/config';
 
 function App() {
+  useEffect(() => {
+
+    const setup = parseFen('r1bqkbnr/ppp2Qpp/2np4/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4').unwrap();
+    const pos = Chess.fromSetup(setup).unwrap();
+    console.assert(pos.isCheckmate());
+  });
+
+  const cfg: Config = {
+    animation: {
+      enabled: true,
+    },
+    coordinates: true,
+    events: {
+      move: function (origin, destination, captured_piece) {
+        console.log("Original move: " + origin + ", destination: " + destination + "captured_piece " + captured_piece);
+      }
+    }
+  }
   return (
     <div className="App">
-      <Chessground />
+      <Chessground config={cfg} />
     </div>
   );
 }
